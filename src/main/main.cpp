e5ebf44cb110be857c60d58a3480e716b32b7971
Dxrmy <119869209+Dxrmy@users.noreply.github.com>
2026-04-01 23:45:53 +0100

    Merge PR #39 and resolve main.cpp conflict

--- src/main/main.cpp
+++ src/main/main.cpp
@@ -539,7 +539,7 @@ int umain (int argn, char** argv)
               // Inject variables
               SD_PRINT("  -> Injecting variables...");
               for (auto& var : project.m_data_win_loader->m_variables) {
-                  ogm::interpreter::staticExecutor.m_frame.m_reflection.add_id(var.name, var.id);
+                  ogm::interpreter::staticExecutor.m_frame.m_reflection.m_namespace_instance.add_id(var.name, var.id);
               }
               SD_PRINT("  -> Variables injected: " + std::to_string(project.m_data_win_loader->m_variables.size()));
 
