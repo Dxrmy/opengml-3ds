acadb711e64b52bbe9fd62678d25901f649d8049
Dxrmy <119869209+Dxrmy@users.noreply.github.com>
2026-04-01 19:52:49 +0100

    debug: Fix BytecodeTable header error and force re-deployment

--- include/ogm/bytecode/BytecodeTable.hpp
+++ include/ogm/bytecode/BytecodeTable.hpp
@@ -326,9 +326,6 @@ public:
         if (!(bytecode_index < m_bytecode.size()))
         {
              std::string err = "BytecodeTable::get_bytecode index " + std::to_string((uint32_t)bytecode_index) + " out of range (size " + std::to_string((uint32_t)m_bytecode.size()) + ")";
-             #ifdef __3DS__
-             SD_PRINT(err);
-             #endif
              throw ogm::AssertionError(err.c_str());
         }
         return m_bytecode.at(bytecode_index);
